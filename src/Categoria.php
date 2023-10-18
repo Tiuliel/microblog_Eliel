383a5f06550fd3092d59e0c9254507cd27035459
Eliel Moura <eliellpf@gmail.com>
2023-10-18 11:45:39 -0300

    exercicio

---
+++ src/Categoria.php
@@ -0,0 +1,92 @@
+<?php 
+namespace Microblog;
+use PDO, Exception;
+
+class Categoria {
+    private int $id;
+    private int $nome;
+    private PDO $conexao;
+
+    public function __construct()
+    {
+        $this->conexao = Banco::conecta();
+    }
+
+    public function ler():array{
+        $sql = "SELECT * FROM categorias ORDER BY nome";
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->execute();
+            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $erro) {
+            die("Erro ao exibir: ".$erro->getMessage());
+        }
+        return $resultado;
+    }
+
+    public function lerUm():array {
+        $sql = "SELECT * FROM categorias WHERE id = :id";
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+            $consulta->execute();
+            $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+        } catch (Exception $erro) {
+            die("Erro ao exibir: ".$erro->getMessage());
+        }
+        return $resultado
+    }
+
+    public function atualizar():void {
+        $sql = "UPDATE categorias SET nome = :nome WHERE id = :id";
+    
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+            $consulta->bindValue(":nome", $this->nome, PDO::PARAM_STR);
+            $consulta->execute();
+        } catch (Exception $erro) {
+            die("Erro ao atualizar usuário: ".$erro->getMessage());
+        }
+    }
+
+    public function excluir():void {
+        $sql = "DELETE FROM categorias WHERE id = :id";
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+            $consulta->execute();
+        } catch (Exception $erro) {
+            die("Erro ao excluir usuário: ".$erro->getMessage());
+        }
+    }
+
+    
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    
+    public function setId(int $id): self
+    {
+        $this->id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+
+        return $this;
+    }
+
+    
+    public function getNome(): int
+    {
+        return $this->nome;
+    }
+
+    
+    public function setNome(int $nome): self
+    {
+        $this->nome = filter_var($nome, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        return $this;
+    }
+}
