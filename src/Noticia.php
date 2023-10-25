386e1b2df96ba75431717f02619ef65880f703a6
Eliel Moura <eliellpf@gmail.com>
2023-10-25 11:54:35 -0300

    atualização

--- src/Noticia.php
+++ src/Noticia.php
@@ -99,7 +99,30 @@ try {
 
 }
 
+public function listarUm():array{
+    if($this->usuario->getTipo() === "admin"){
+        $sql = "SELECT * FROM noticias WHERE id = :id";
+    } else {
+        $sql = "SELECT * FROM noticias
+                WHERE id = :id AND usuario_id = :usuario_id";
+    }
 
+    try {
+        $consulta = $this->conexao->prepare($sql);
+        $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+    
+        if( $this->usuario->getTipo() !== "admin" ){
+            $consulta->bindValue(":usuario_id", $this->usuario->getId(), PDO::PARAM_INT);
+        }
+        
+        $consulta->execute();
+        $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+    
+    } catch (Exception $erro) {
+        die("Erro ao carregar notícias: ".$erro->getMessage());
+    }
+        return $resultado;
+}
 
 
     /* Método para upload de foto */
