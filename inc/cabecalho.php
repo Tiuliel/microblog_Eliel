3e04f58e53931e8bb9b8d6ec53bd7d606361fead
Eliel Moura <eliellpf@gmail.com>
2023-10-26 12:01:21 -0300

    cabeçalho

--- inc/cabecalho.php
+++ inc/cabecalho.php
@@ -1,8 +1,9 @@
 <?php 
 require_once "vendor/autoload.php";
 use Microblog\Noticia;
-use Microblog\Utilitarios;
 $noticia = new Noticia;
+
+$listaDeCategorias = $noticia->categoria->listar();
 ?>
 <!DOCTYPE html>
 <html lang="pt-br" class="h-100">
@@ -39,9 +40,10 @@ $noticia = new Noticia;
             Categorias
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="noticias-por-categoria.php">Ciência</a></li>
-            <li><a class="dropdown-item" href="noticias-por-categoria.php">Educação</a></li>
-            <li><a class="dropdown-item" href="noticias-por-categoria.php">Tecnologia</a></li>
+
+          <?php foreach ($listaDeCategorias as $itemCategoria){?>
+            <li><a class="dropdown-item" href="noticias-por-categoria.php?id=<?=$itemCategoria["id"]?>"><?=$itemCategoria["nome"]?></a></li>
+            <?php } ?>
           </ul>
         </li>
         <li class="nav-item">
